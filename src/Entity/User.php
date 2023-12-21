e3d62cc8c95040a7685fa6a09431304801703f9c
lasuc <115156956+lasuc@users.noreply.github.com>
2023-12-21 23:08:39 +0100

    acquired_session

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,16 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Scheb\TwoFactorBundle\Model\Email\TwoFactorInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements PasswordAuthenticatedUserInterface
+#[UniqueEntity(fields: ['Email'], message: 'There is already an account with this Email')]
+class User implements PasswordAuthenticatedUserInterface, UserInterface, TwoFactorInterface
+
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -21,6 +25,12 @@ class User implements PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     private ?string $Password = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $authCode;
 
     public function getId(): ?int
     {
@@ -49,6 +59,59 @@ class User implements PasswordAuthenticatedUserInterface
         $this->Password = $Password;
 
         return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Vous n'avez généralement pas besoin de faire quelque chose ici, sauf si vous stockez des informations sensibles.
+    }
+
+    public function getRoles(): array
+    {
+        // Retournez un tableau de rôles, par exemple ['ROLE_USER']
+        return ['ROLE_USER'];
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+    
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function isEmailAuthEnabled(): bool
+    {
+        return true; // This can be a persisted field to switch email code authentication on/off
+    }
+
+    public function getEmailAuthRecipient(): string
+    {
+        return $this->email;
+    }
+
+    public function getEmailAuthCode(): string
+    {
+        if (null === $this->authCode) {
+            throw new \LogicException('The email authentication code was not set');
+        }
+
+        return $this->authCode;
+    }
+
+    public function setEmailAuthCode(string $authCode): void
+    {
+        $this->authCode = $authCode;
     }
 
 }
